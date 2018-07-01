439e8730517490ebcd22c2b22cde6c91b50d0856
Alex Lee <alexgrlee@gmail.com>
2018-07-01 20:37:10 +1200

    Tiny ass typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { prefix, token, } = require('./config.json');
+const { prefix, token } = require('./config.json');
 
 let forbiddenRanks = [];
 forbiddenRanks.push("Admins", "Vanity", "baaa-admin", "vuwbotbestbot", 
