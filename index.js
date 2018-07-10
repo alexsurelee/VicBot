4a001fdda7e0d4d47c1747b3e8416c9e44321905
Alex Lee <alexgrlee@gmail.com>
2018-07-10 13:51:49 +1200

    no real changes, reset broke on its own and angery

--- index.js
+++ index.js
@@ -495,8 +495,8 @@ async function createRank(message, args) {
  * @param {string[]} args
  */
 async function deleteRank(message, args) {
-    if (message.guild.roles.find(role => role.name === args[0]) != null) await message.guild.roles.find(role => role.name === args[0]).delete();
-    if (message.guild.channels.find(channel => channel.name === args[0]) != null) await message.guild.channels.find(channel => channel.name === args[0]).delete();
+    if (message.guild.roles.find(role => role.name === args[0]) !== null) await message.guild.roles.find(role => role.name === args[0]).delete();
+    if (message.guild.channels.find(channel => channel.name === args[0]) !== null) await message.guild.channels.find(channel => channel.name === args[0]).delete();
     return;
 }
 
