093b70c7ea030b1f49077a89b9171928621e273e
Alex Lee <alexgrlee@gmail.com>
2018-07-10 17:10:32 +1200

    All looks good.

--- commands/alias.js
+++ commands/alias.js
@@ -7,7 +7,7 @@ module.exports = {
 	usage: `\`!alias <alias> <course> [course ...]\``,
 	description: `Changes the papers allocated to an alias.`,
 	async execute(message, args){
-		if (message.guild.roles.find(role => role.name === args[0]) === null) {
+		if (!message.guild.roles.some(role => role.name === args[0])) {
 			return message.channel.send(`Couldn't find ${args[0]}`);
 		}
 		else if (aliasRanks.indexOf(args[0]) === -1) {
@@ -16,11 +16,11 @@ module.exports = {
 		else {
 			var role = message.guild.roles.find(role => role.name === args[0]);
 			for (let i = 1; i < args.length; i++){
-				if (role.guild.channels.find(channel => channel.name === args[i]) === null) return message.channel.send(`Couldn't find ${args[i]}`);
+				if (role.guild.channels.find(channel => channel.name === args[i]) === null){ message.channel.send(`Couldn't find ${args[i]}`); continue; }
 				var channel = role.guild.channels.find(ch => ch.name === args[i]);
 				if (role.permissionsIn(channel).has(`VIEW_CHANNEL`)) {
 					await channel.updateOverwrite(role, { VIEW_CHANNEL: null });
-					return message.channel.send(`Removed ${role.name} from ${args[i]}`);
+					message.channel.send(`Removed ${role.name} from ${args[i]}`);
 				}
 				else {
 					await channel.updateOverwrite(role, { VIEW_CHANNEL: true });
