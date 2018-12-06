aff97580989e7206a2089aeea939b9d2932c4f47
Alex Lee <alexgrlee@gmail.com>
2018-12-06 18:21:48 +1300

    Removed the 'multiple ranks' message

--- commands/rank.js
+++ commands/rank.js
@@ -1,13 +1,12 @@
-const index = require("../index.js");
+const index = require(`../index.js`);
 
 module.exports = {
-    name: `rank`,
+	name: `rank`,
     aliases: [`role`],
     args: true,
     description: `Add or remove class ranks.`,
     usage: `\`!rank <course> [course ...]\``,
     async execute(message, args){
-        if(args.length === 1) message.channel.send(`Note you can add and remove multiple ranks in one command! e.g. \`!rank comp-102 comp-103\``);
         args.forEach(function(rank) {
 			index.rank(message, rank);
 		});
