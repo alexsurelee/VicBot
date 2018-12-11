42a1350a27c394de630b55f8a4cb831eba6481a1
Alex Lee <alexgrlee@gmail.com>
2018-12-11 17:52:12 +1300

    Stopped reset from removing the role

--- commands/reset.js
+++ commands/reset.js
@@ -1,33 +1,34 @@
 const { forbiddenRanks } = require(`../config.json`);
-const index = require("../index.js");
+const index = require(`../index.js`);
 
 module.exports = {
-    name: 'reset',
-    admin: true,
-    args: true,
-    description: `Resets the channel and role for a course.`,
-    usage: `\`!reset <course>\``,
-    async execute(message, args){
-        if (!args.length) {
-            return message.channel.send(`Please include the name of the channel you wish to reset.`);
-        }
-    
-        else if (args.length > 1) {
-            return message.channel.send(`Please only include one rank to reset (no spaces).`);
-        }
-    
-        else if ((message.guild.roles.find(role => role.name === args[0]) == null) && (message.guild.channels.find(channel => channel.name === args[0]) == null)) {
-            return message.channel.send(`Cannot find rank to reset.`);
-        }
-    
-        else if (forbiddenRanks.includes(args[0])) {
-            return message.channel.send(`You probably shouldn't reset this, and at the moment I'm not going to let you.`);
-        }
-    
-        else {
-            await index.deleteRank(message, args);
-            await index.newRank(message, args);
-            return message.channel.send(`Reset ${args[0]}.`);
-        }
-    }
-}
+	name: `reset`,
+	admin: true,
+	args: true,
+	description: `Resets the channel and role for a course.`,
+	usage: `\`!reset <course>\``,
+	async execute(message, args){
+		if (!args.length) {
+			return message.channel.send(`Please include the name of the channel you wish to reset.`);
+		}
+
+		else if (args.length > 1) {
+			return message.channel.send(`Please only include one rank to reset (no spaces).`);
+		}
+
+		else if ((message.guild.roles.find(role => role.name === args[0]) == null) && (message.guild.channels.find(channel => channel.name === args[0]) == null)) {
+			return message.channel.send(`Cannot find rank to reset.`);
+		}
+
+		else if (forbiddenRanks.includes(args[0])) {
+			return message.channel.send(`You probably shouldn't reset this, and at the moment I'm not going to let you.`);
+		}
+
+		else {
+			// await index.deleteRank(message, args);
+			if (message.guild.channels.find(channel => channel.name === args[0]) != null) await message.guild.channels.find(channel => channel.name === args[0]).delete();
+			await index.newRank(message, args);
+			return message.channel.send(`Reset ${args[0]}.`);
+		}
+	},
+};
