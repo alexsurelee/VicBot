0e556717c1d5f3c78c944446452775781e889806
Alex Lee <alexgrlee@gmail.com>
2019-03-07 12:07:11 +1300

    cleanup

--- commands/micropad.js
+++ commands/micropad.js
@@ -4,15 +4,8 @@ module.exports = {
 	usage: `\`!micropad\``,
 	description: `Provides information about micropad.`,
 	async execute(message) {
-		if(message.author.id === `125855147610865664`) {
-			message.channel.send(
-				`<:micropad:339927818181935105> is the easy to use powerful notepad app developed by our very own Nick. Check it out at https://getmicropad.com`
-			);
-		}
-		else {
-			message.channel.send(
-				`<:micropad:339927818181935105> Micropad is the best notepad app. Nobody makes notepad apps as good as me. Check it out at https://getmicropad.com`
-			);
-		}
+		message.channel.send(
+			`<:micropad:339927818181935105> is the easy to use powerful notepad app developed by our very own Nick. Check it out at https://getmicropad.com`
+		);
 	},
 };
