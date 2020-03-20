31ef456d2f45b48f1600b2fe738b5a0c6d8a8dc2
Alex <alexgrlee@gmail.com>
2020-03-21 11:15:45 +1300

    Added commands for adding/removing social ranks

---
+++ commands/addsocial.js
@@ -0,0 +1,48 @@
+const index = require("../index.js");
+module.exports = {
+  name: "addsocial",
+  args: true,
+  admin: true,
+  log: true,
+  description: "Creates a new social rank.",
+  usage: "`!addsocial <name>`",
+  async execute(message, args) {
+    args[0] = args[0].toLowerCase();
+    if (!args.length) {
+      return message.channel.send(
+        "Please include the name of the rank you wish to create."
+      );
+    }
+    else if (args.length > 1) {
+      return message.channel.send(
+        "Please only include one rank to create (no spaces)."
+      );
+    }
+    else if (args[0].includes("+")) {
+      return message.channel.send(
+        "You cannot include the `+` symbol in a rank."
+      );
+    }
+    else if (
+      message.guild.roles.find(role => role.name === args[0]) != null
+    ) {
+      return message.channel.send(
+        "Couldn't create rank - role already exists."
+      );
+    }
+    else if (
+      message.guild.channels.find(role => role.name === args[0]) != null
+    ) {
+      return message.channel.send(
+        "Couldn't create rank - channel already exists."
+      );
+    }
+    else {
+      await index.newSocial(message, args);
+      const rankChannel = message.guild.channels.find(
+        channel => channel.name === args[0]
+      );
+      return message.channel.send(`Created ${rankChannel} successfully.`);
+    }
+  }
+};
