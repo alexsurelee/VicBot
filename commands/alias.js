0590693161e213d6e972b8625a98e7a51ba9afb4
Alex <alexgrlee@gmail.com>
2020-03-20 19:03:27 +1300

    Made aliases add/remove individual ranks, rather than existing on their own

--- commands/alias.js
+++ commands/alias.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const index = require("../index.js");
+const fs = require("fs");
 
 module.exports = {
   name: "alias",
@@ -9,29 +10,20 @@ module.exports = {
   usage: "`!alias <alias>`",
   description: "Lists the papers allocated to an alias.",
   async execute(message, args) {
-    const aliasRegex = /^\d{3}-level$/;
     for (let i = 0; i < args.length; i++) {
-      if (!message.guild.roles.some(role => role.name === args[i])) {
-        message.channel.send(`Couldn't find ${args[i]}`);
+      if (!index.aliasRegex.test(args[i])) {
+        message.channel.send(`${args[i]} is not an alias.`);
         continue;
       }
-      if (!aliasRegex.test(args[i])) {
-        message.channel.send(`${args[0]} is not an alias.`);
-        continue;
-      }
-      const role = message.guild.roles.find(role => role.name === args[i]);
       let channels = "```";
       let count = 1;
-      message.guild.channels.array().forEach(channel => {
-        if (role.permissionsIn(channel).has("VIEW_CHANNEL")) {
-          if (index.isPaper(channel)) {
-            channels += channel.name;
-            if (count % 4 === 0) channels += "\n";
-            else channels += "\t";
-            count++;
-          }
-        }
-      });
+      if (fs.existsSync(__dirname + "/../data/aliases.json")) {
+        const aliases = require(__dirname + "/../data/aliases.json");
+        channels += aliases[args[i]];
+        if (count % 4 === 0) channels += "\n";
+        else channels += "\t";
+        count++;
+      }
       channels += "```";
       message.channel.send(
         new Discord.MessageEmbed().setTitle(args[i]).setDescription(channels)
